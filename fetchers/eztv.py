3f8c5cd48c3a112afff38a5ca457de8baa4c9cdf
Furkan Varol <furkanvarol@gmail.com>
2015-01-28 23:41:47 +0200

    eztv fetcher implementation: * eztv api has been fixed * eztv fetcher has been implemented * sample show_watchlist has been added * horriblesubs season finder with start with s condition has been fixed * update_last_download method has been added in main * utils init py has been added * pyc files has been removed from git tracked list

--- fetchers/eztv.py
+++ fetchers/eztv.py
@@ -1,9 +1,34 @@
-from eztv_api import EztvAPI
+import json
 
-def get_links():
-  test_api = EztvAPI().tv_show('perception')
-  seasons = test_api.seasons()
-  for season in seasons:
-      for episode in seasons[season]:
-          # will print the magnet link of all episodes, in all seasons
-          print seasons[season][episode]
+from eztv_api import EztvAPI
+from eztv_api import TVShowNotFound
+
+#check shows from EZTV.ch
+def get_links(watchlist):
+  links = []
+
+  for show in watchlist:
+    show_name = show["name"]
+    (last_season, last_episode) = show["lastDownloaded"].split("-")
+
+    last_season = int(last_season)
+    last_episode = int(last_episode)
+
+    try:
+      seasons = EztvAPI().tv_show(show_name).seasons()
+      for season in seasons:
+        for episode in seasons[season]:
+          if (season > last_season or (season == last_season and episode > last_episode)):
+            season_name = "0" + str(season) if season < 10 else str(season)
+            episode_name = "0" + str(episode) if episode < 10 else str(episode)
+            magnet = seasons[season][episode]
+
+            links.append((show_name, season_name, episode_name, magnet))
+    except TVShowNotFound as e:
+      print e
+      pass
+    except Exception as e: 
+      #do nothing since show is found
+      pass
+
+  return links
